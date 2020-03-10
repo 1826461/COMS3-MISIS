87d14e60d869edb94ee0594c26af60b733965f42
tleforestier99@gmail.com <tleforestier99@gmail.com>
2020-03-10 10:43:30 +0200

    Created Detail.php

---
+++ CRUD/Detail.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Manage Database</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" />
+
+    <!-- custom css -->
+    <style>
+        .m-r-1em{ margin-right:1em; }
+        .m-b-1em{ margin-bottom:1em; }
+        .m-l-1em{ margin-left:1em; }
+        .mt0{ margin-top:0; }
+    </style>
+
+</head>
+<body>
+    <!--Container-->
+    <div class="container">
+            <div class="page-header">
+                <h1>Read Users</h1>
+            </div>
+    <!-- PHP code for read records here-->
+        <?php
+        // Add code to connect to database
+
+
+        ?>
+
+    </div> <!-- end of container-->
+
+<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+<script src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
+
+<!-- Latest compiled and minified Bootstrap JavaScript -->
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+
+<!-- confirm delete record will be here -->
+
+
+
+</body>
+</html>
