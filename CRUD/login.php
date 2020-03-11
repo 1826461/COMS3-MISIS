34e5b32f0bad8dcedc4f3bb73240cdab9595669b
tleforestier99@gmail.com <tleforestier99@gmail.com>
2020-03-11 19:16:45 +0200

    fixed passwords

--- CRUD/login.php
+++ CRUD/login.php
@@ -11,33 +11,27 @@ session_start();
 
 <body>
 <form class="Login" method="post">
-    <div class="form-container">
-        <ul class="list">
-            <li>User Login</li>
-            <li><input type="text" name="UserName" placeholder="User Name" ></li>
-            <li><input type="password" name="Password" placeholder="Password" ></li>
-            <li><input type="submit" name="Login" value="Login"></li>
-        </ul>
-    </div>
     <?php
      include 'database.php';
     $username ="";
     $password="";
     $hashPassword="";
+    $usernameErr="";
+    $passwordErr="";
     if(isset($_POST["Login"])) {
         //check empty username
         if(empty($_POST["UserName"])){
-
+            $usernameErr = "Username is required.";
         }else{
             $username = $_POST["UserName"];
         }
         //check blank password
         if(empty($_POST["Password"])){
-
+            $passwordErr = "Password is required.";
         }else{
             $password = $_POST["Password"];
-            //echo "<p>$hashPassword</p>";
-           //query DB
+        }
+        if (!(empty($_POST["UserName"])&&empty($_POST["Password"]))){
             try {
                 $data = "SELECT * FROM userroles WHERE userID = ? LIMIT 0,1";
                 $stmt = $dbh->prepare($data);
@@ -47,17 +41,15 @@ session_start();
                 // extract($row);
                 $numRows = $stmt->rowCount();
                 if ($numRows==0){
-
+                    $usernameErr="User not found";
                 }else{
                     $userPassword = $row['password'];
-                    echo "<p>$hashPassword</p>";
-                    echo "<p>$userPassword</p>";
                     if (password_verify($password,$userPassword)){
                         $_SESSION['loggedin'] = true;
                         $_SESSION['username'] = $username;
                         header("location: Detail.php");
                     }else{
-
+                    $passwordErr="Incorrect Password";
                     }
                 }
             }catch (PDOException $exception){
@@ -68,6 +60,14 @@ session_start();
 
     }
     ?>
+    <div class="form-container">
+        <ul class="list">
+            <li>User Login</li>
+            <li><input type="text" name="UserName" placeholder="User Name" ><span class="error">* <?php echo"<p>$usernameErr</p>";?></span></li>
+            <li><input type="password" name="Password" placeholder="Password" ><span class="error">* <?php echo"<p> $passwordErr</p>";?></span></li>
+            <li><input type="submit" name="Login" value="Login"></li>
+        </ul>
+    </div>
 </form>
 </body>
 
@@ -85,12 +85,17 @@ session_start();
     .form-container{
         width: 250px;
         height: auto;
-        padding: 20px 30px;
-        background-color: white ;
+        padding: 30px 100px;
+        background-color: beige ;
         border-radius: 10px;
         box-shadow: 0 0 10px #000;
         margin: auto;
     }
+
+    p{
+        padding-top: 2px;
+    }
+
     ul.list{
         list-style: none;
         text-align: center;
