d3e57c762c1cc5acdb514d63708b38d985048a41
tleforestier99@gmail.com <tleforestier99@gmail.com>
2020-03-26 21:37:16 +0200

    Fixed database.php when no connection is established

--- CRUD/database.php
+++ CRUD/database.php
@@ -9,6 +9,7 @@ $dbname="coms3project";
 try {
     $dbh = new PDO("mysql:host={$host};port={$port};dbname={$dbname}", $user, $password);
 } catch (PDOException $e) {
-    echo 'Connection failed: ' . $e->getMessage();
+//    echo 'Connection failed: ' . $e->getMessage();
+    echo "<script>window.alert('No connection to database established. Please contact your local database administrator.')</script>";
 }
 
