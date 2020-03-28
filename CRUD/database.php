7fd9d04c6cf368eedce157708f1c863276d2b029
tleforestier99@gmail.com <tleforestier99@gmail.com>
2020-03-28 14:53:08 +0200

    changed login user

--- CRUD/database.php
+++ CRUD/database.php
@@ -1,15 +1,15 @@
 <?php
+
 $host="localhost";
 $port=3306;
 $socket="";
-$user="Test";
-$password="testing1234";
+$user="projectUser";
+$password="COMSPROJECT2020";
 $dbname="coms3project";
 
 try {
-    $dbh = new PDO("mysql:host={$host};port={$port};dbname={$dbname}", $user, $password);
+$dbh = new PDO("mysql:host={$host};port={$port};dbname={$dbname}", $user, $password);
 } catch (PDOException $e) {
-//    echo 'Connection failed: ' . $e->getMessage();
-    echo "<script>window.alert('No connection to database established. Please contact your local database administrator.')</script>";
+echo 'Connection failed: ' . $e->getMessage();
 }
 
