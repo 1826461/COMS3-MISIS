ea9e01359016c803c23a845049cd39bba37d8781
Tristen Paul <1826461@students.wits.ac.za>
2020-05-24 21:20:45 +0200

    Added functionality to delete all enrollments from a particular course and JSON helper to access and process Virtus data

---
+++ src/Helpers/JSONHelper.php
@@ -0,0 +1,46 @@
+<?php
+
+
+namespace Helpers;
+
+use Objects\Enrollment;
+use PHPUnit\Util\Json;
+
+class JSONHelper
+{
+    //TODO TEST FUNCTIONALITY WITH VM - LOCAL TESTS PASS
+    function parseEnrollmentJSON(string $json) {
+        $enrollmentDatabaseHelper = new EnrollmentDatabaseHelper();
+        $enrollmentJSONArray = json_decode($json, true);
+        foreach ($enrollmentJSONArray as $enrollmentJSON) {
+            $enrollment = new Enrollment(0, $enrollmentJSON['studentNumber'], $enrollmentJSON['firstName'],
+                $enrollmentJSON['surname'], $enrollmentJSON['subject'], $enrollmentJSON['unitCode'],
+                $enrollmentJSON['sessionCode'], $enrollmentJSON['classSection'], $enrollmentJSON['expiryDate'],
+                $enrollmentJSON['unitStatus']);
+
+            $enrollmentDatabaseHelper->insertEnrollment($enrollment);
+
+        }
+
+    }
+
+    function getVirtusCourseJSON(string $unitCode) {
+
+        $url = 'http://wims-service-user:w\!im5-5erv1s-u5er@virtus.wits.ac.za:8180/wits-wims-services/wims/student/unitStudents/';
+        $url = $url . $unitCode .'/';
+        $curl = curl_init();
+        curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+        $result = curl_exec($curl);
+        curl_close($curl);
+        return $result;
+    }
+    //TODO WHEN GIVEN ACCESS
+    function updateCourseData(string $unitCode) {
+        $enrollmentDatabaseHelper = new EnrollmentDatabaseHelper();
+        $data = self::getVirtusCourseJSON($unitCode);
+        $enrollmentDatabaseHelper->deleteAllCourseEnrollments($unitCode);
+        self::parseEnrollmentJSON($data);
+
+    }
+
+}
---
+++ tests/Helpers/JSONHelperTest.php
@@ -0,0 +1,34 @@
+<?php
+
+use Helpers\DatabaseHelper;
+use Helpers\EnrollmentDatabaseHelper;
+use Helpers\JSONHelper;
+use PHPUnit\Framework\TestCase;
+use function PHPUnit\Framework\assertEquals;
+
+class JSONHelperTest extends TestCase
+{
+    private static DatabaseHelper $databaseHelper;
+    private static string $json;
+    public function testParseJSONEnrollment() {
+        $jsonHelper = new JSONHelper();
+        self::$json = '[{"subject": "COMS","surname": "Komape","firstName": "Kabelo","unitStatus": "ENROLLED",
+        "unitCode": "COMS3007A","studentNumber": "676400","expiryDate": "2020-06-30 00:00:00",
+        "sessionCode": "SM1","classSection": "D"}]';
+        self::$databaseHelper = new databaseHelper();
+        $jsonHelper->parseEnrollmentJSON(self::$json);
+
+        $enrollmentDatabaseHelper = new EnrollmentDatabaseHelper();
+        $result = $enrollmentDatabaseHelper->getEnrollment(676400, "COMS3007A");
+        assertEquals($result->getStudentNo(),676400, "correct student number returned");
+        assertEquals($result->getName(),"Kabelo", "correct name returned");
+        assertEquals($result->getSurname(),"Komape", "correct surname returned");
+        assertEquals($result->getExpiryDate(),"2020-06-30 00:00:00", "correct expiry date returned");
+        assertEquals($result->getSession(),"SM1", "correct session returned");
+        assertEquals($result->getSubject(),"COMS", "correct subject returned");
+        assertEquals($result->getClassSection(),"D", "correct class section returned");
+
+        $enrollmentDatabaseHelper->deleteEnrollment(676400, "COMS3007A");
+    }
+
+}
