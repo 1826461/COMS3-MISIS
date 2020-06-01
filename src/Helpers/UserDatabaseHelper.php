204c75066471cb9e7cf61cfbd20b4a1c3345b9bb
Ramtin Mesgari <ramtin.mesgari@gmail.com>
2020-06-01 13:59:13 +0200

    Added try/catch code to UserDatabaseHelper.php

--- src/Helpers/UserDatabaseHelper.php
+++ src/Helpers/UserDatabaseHelper.php
@@ -4,17 +4,23 @@
 namespace Helpers;
 
 use Objects\User;
+use PDOException;
 
 class UserDatabaseHelper
 {
 
     public static function getUser($username) {
         $databaseHelper = new DatabaseHelper();
-        $databaseHelper->query("SELECT * FROM users WHERE userID = ? LIMIT 0,1");
-        $databaseHelper->bind(1, $username);
-        $user = $databaseHelper->single();
-        if ($databaseHelper->rowCount() === 0) {
-            return 0;
+
+        try {
+            $databaseHelper->query("SELECT * FROM users WHERE userID = ? LIMIT 0,1");
+            $databaseHelper->bind(1, $username);
+            $user = $databaseHelper->single();
+            if ($databaseHelper->rowCount() === 0) {
+                return 0;
+            }
+        } catch (PDOException $e) {
+            $databaseHelper->error = $e->getMessage();
         }
         return new User($user['userID'], $user['password'], $user['role']);
     }
