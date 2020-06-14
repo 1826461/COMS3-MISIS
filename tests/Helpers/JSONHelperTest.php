9cb7950b47c440f601e48897042c057030f070bf
Tristen Paul <1826461@students.wits.ac.za>
2020-06-14 02:58:38 +0200

    Add JSON Helper Tests

---
+++ tests/Helpers/JSONHelperTest.php
@@ -0,0 +1,28 @@
+<?php
+
+use Helpers\JSONHelper;
+use Helpers\EnrollmentDatabaseHelper;
+
+use PHPUnit\Framework\TestCase;
+use function PHPUnit\Framework\assertTrue;
+
+class JSONHelperTest extends TestCase
+{
+    public function testAddCourseData() {
+        $JSONHelper = new JSONHelper();
+        $work = $JSONHelper->addCourseData("COMS1015");
+        assertTrue($work, "connected to Virtus API and updated entries");
+    }
+
+    public function testUpdateCourseData() {
+        $JSONHelper = new JSONHelper();
+        $JSONHelper->updateCourseData("COMS1015");
+        $work = $JSONHelper->updateCourseData("COMS1015");
+        assertTrue($work, "connected to Virtus API and updated entries");
+        $enrollmentDatabaseHelper = new EnrollmentDatabaseHelper();
+        $enrollmentDatabaseHelper->deleteAllCourseEnrollments("COMS1015");
+
+    }
+
+
+}
