be83ca56162d3c7bd20acf3236dd22930a2d60c4
Tristen Paul <1826461@students.wits.ac.za>
2020-10-09 16:44:45 +0200

    Fixed Updates.php to selectively insert and delete

--- src/WebAccess/WebAPI/Updates.php
+++ src/WebAccess/WebAPI/Updates.php
@@ -25,40 +25,45 @@ function triggerSync($unitCode, $delActive){
     $courseHelper = new CourseDatabaseHelper();
     $course = $courseHelper->getCourse($unitCode);
 
-    if($delActive==0){//Only do new user additions
+    $courseAdditionCount = 0;
+    $courseDeletionCount = 0;
+
+
+    //Only do new user additions
         //Get current virtus enrollments
         $enrollAdd = $JSONHelper->getVirtusCourseJSON($unitCode);
-
         for ($index = 0; $index < sizeof($enrollAdd);$index++) {
             //need to get an ID for insert
             $insertID = 0;
             $enrollment = new Enrollment($insertID,$enrollAdd[$index]['studentNumber'],$enrollAdd[$index]['firstName'], $enrollAdd[$index]['surname'], $enrollAdd[$index]['subject'], $enrollAdd[$index]['unitCode'], $enrollAdd[$index]['sessionCode'], $enrollAdd[$index]['classSection'], $enrollAdd[$index]['expiryDate'], $enrollAdd[$index]['unitStatus']);
             $courseID = $course->getCourseID();
-            $enrollmentDatabaseHelper->insertUniqueEnrollmentWithCourseID($enrollment,$courseID);
-        }
-        $logEntry = new LogEntry("CRON", sizeof($enrollAdd) . " enrollments added to " . $course->getCourseName());
-        $logEntryDatabaseHelper->insertLogEntry($logEntry);
-    }else if ($delActive==1){//Delete old users and insert new users
-
-        //Delete old enrollments from specific course
-        $enrollDel = $enrollmentDatabaseHelper->getAllEnrollments();
-        for ($index = 0; $index < sizeof($enrollDel); $index++) {
-            if ($enrollDel[$index]['unitCode']===$unitCode){
-                $enrollmentDatabaseHelper->deleteEnrollment($enrollDel[$index]['studentNo'], $enrollDel[$index]['unitCode']);
+            $curr = $enrollmentDatabaseHelper->getEnrollment($enrollAdd[$index]['studentNumber'], $enrollAdd[$index]['unitCode']);
+            if ($curr == 0){
+                $enrollmentDatabaseHelper->insertUniqueEnrollmentWithCourseID($enrollment,$courseID);
+                $courseAdditionCount = $courseAdditionCount + 1;
             }
         }
-        $logEntry = new LogEntry("CRON", sizeof($enrollDel) . " enrollments deleted from " . $course->getCourseName());
+        $logEntry = new LogEntry("CRON", $courseAdditionCount . " enrollments added to " . $course->getCourseName());
         $logEntryDatabaseHelper->insertLogEntry($logEntry);
-        //Get current virtus enrollments
-        $enrollAdd = $JSONHelper->getVirtusCourseJSON($unitCode);
-        for ($index = 0; $index < sizeof($enrollAdd);$index++) {
-            //need to get an ID for insert
-            $insertID = 0;
-            $enrollment = new Enrollment($insertID,$enrollAdd[$index]['studentNumber'],$enrollAdd[$index]['firstName'], $enrollAdd[$index]['surname'], $enrollAdd[$index]['subject'], $enrollAdd[$index]['unitCode'], $enrollAdd[$index]['sessionCode'], $enrollAdd[$index]['classSection'], $enrollAdd[$index]['expiryDate'], $enrollAdd[$index]['unitStatus']);
-            $courseID = $course->getCourseID();
-            $enrollmentDatabaseHelper->insertEnrollmentWithCourseID($enrollment,$courseID);
+    if ($delActive==1){//Delete old users and insert new users
+
+        for ($index = 0; $index < sizeof($enrollAdd); $index++) {
+            $enrollment = new Enrollment(0,$enrollAdd[$index]['studentNumber'],$enrollAdd[$index]['firstName'], $enrollAdd[$index]['surname'], $enrollAdd[$index]['subject'], $enrollAdd[$index]['unitCode'], $enrollAdd[$index]['sessionCode'], $enrollAdd[$index]['classSection'], $enrollAdd[$index]['expiryDate'], $enrollAdd[$index]['unitStatus']);
+            $enrollmentDatabaseHelper->insertTempEnrollment($enrollment);
         }
-        $logEntry = new LogEntry("CRON", sizeof($enrollAdd) . " enrollments added to " . $course->getCourseName());
+
+        //Delete old enrollments from specific course
+        $enrollDel = $enrollmentDatabaseHelper->getAllEnrollmentsWhereNotInTemp();
+
+        if ($enrollDel != 0) {
+            for ($index = 0; $index < sizeof($enrollDel); $index++) {
+                if ($enrollDel[$index]['unitCode']===$unitCode){
+                    $enrollmentDatabaseHelper->deleteEnrollment($enrollDel[$index]['studentNo'], $enrollDel[$index]['unitCode']);
+                    $courseDeletionCount = $courseDeletionCount + 1;
+                }
+            }
+        }
+        $logEntry = new LogEntry("CRON", $courseDeletionCount . " enrollments deleted from " . $course->getCourseName());
         $logEntryDatabaseHelper->insertLogEntry($logEntry);
     }
     //once finished update last sync in Database
